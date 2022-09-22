daa30d7825f58bf6e075482b03a5dc96819016a7
Deluxe Duck <deluxe.duck@yahoo.com>
2022-09-22 15:48:30 +0200

    app-alpha-build

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,9 @@ function Header() {
     return (
         <div className='header'>
 
+            <div className='header_left'></div>
+            <div className='header_right'></div>
+
         </div>
     );
 }
