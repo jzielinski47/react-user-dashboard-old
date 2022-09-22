1664a5736ea8f572785739332e6b6f89867cfc31
Deluxe Duck <deluxe.duck@yahoo.com>
2022-09-22 16:27:37 +0200

    user-initials-update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,46 @@
 import { Avatar } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import { deepOrange } from '@mui/material/colors';
+import './Header.css';
 import React from 'react';
 
+interface Account {
+    id?: string;
+    name: string;
+    description: string;
+}
+
 function Header() {
+
+    const user: Account = {
+        name: "John Snow",
+        description: "Project Manager"
+    }
+
+    const getInitials = (name: string) => {
+        const names = name.split(' ')
+        let initials: string = "";
+        for (let i = 0; i < names.length; i++) {
+            initials += names[i].charAt(0)
+        }
+        return initials;
+    }
+
     return (
         <div className='header'>
             <div className='header_left'>
-                Dashboard
+                <MenuIcon /> Dashboard
             </div>
             <div className='header_right'>
-                <Avatar>JZ</Avatar>
+                <div className='user-account'>
+                    <Avatar sx={{ bgcolor: deepOrange[500] }}>{getInitials(user.name)}</Avatar>
+                    <div className='account-details'>
+                        <span className='username '>{user.name}</span>
+                        <span className='user-description'>{user.description}</span>
+                    </div>
+
+                </div>
+
             </div>
         </div>
     );
