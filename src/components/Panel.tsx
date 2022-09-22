1612315e83c448ae5f465d1fc64b3810147e6031
Deluxe Duck <deluxe.duck@yahoo.com>
2022-09-22 23:30:30 +0200

    panel-color-update

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -5,9 +5,10 @@ import { PanelProperities } from '../interfaces/interfaces';
 function Panel(props: PanelProperities) {
 
     let [selected, select] = useState(false);
+    let [background, setBackground] = useState('');
 
     return (
-        <div className='panel' onClick={() => select(selected = !selected)}>
+        <div className='panel' onClick={() => { select(selected = !selected); console.log(selected); }}>
             <span className='title'>{props.title}</span>
         </div >
     );
