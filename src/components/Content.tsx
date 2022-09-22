35b8eb4fe2e9b4ca3565c4838bca225dd8ead4cd
Deluxe Duck <deluxe.duck@yahoo.com>
2022-09-22 23:07:02 +0200

    content-update

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -4,11 +4,11 @@ import Panel from './Panel';
 import React from 'react';
 
 
-const panels: PanelProperities[] = [
-    { id: 0, title: `panel 0` },
-    { id: 1, title: `panel 1` },
-    { id: 2, title: `panel 2` },
-]
+let panels: PanelProperities[] = []
+
+for (let i = 0; i < 5; i++) {
+    panels.push({ id: i, title: `panel ${i}` })
+}
 
 function Content() {
     return (
